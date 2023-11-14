9622c1da5335bcd85a359d11911f67ee2e8e5e91
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-11-14 12:06:28 +0100

    P4.4.py

--- P4.4.py
+++
@@ -1,9 +0,0 @@
-def removeLastChar(stringParam):
-    if len(stringParam) <= 1:
-        return stringParam
-    else:
-        return stringParam[:-1]
-
-stringParam = input("Enter a string: ")
-result = removeLastChar(stringParam)
-print("Modified string:", result)
